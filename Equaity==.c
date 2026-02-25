589c6d5b26e320de56be6b26d55e58cf659960e0
saikiran-2905 <saikiraneswarapragada@gmail.com>
2026-02-25 20:12:12 +0530

    Add files via upload

---
+++ Equaity==.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if (a == b) {
+        printf("Both numbers are Equal\n");
+    } else {
+        printf("Numbers are Not Equal\n");
+    }
+
+    return 0;
+}
